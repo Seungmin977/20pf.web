27f425bdf9e22ce995c18261a4fb4cc3353de655
nable-kata <55433953+Seungmin977@users.noreply.github.com>
2021-01-11 21:31:37 +0900

    Add files via upload

---
+++ StarBucksSite/js/image.js
@@ -0,0 +1,15 @@
+
+function image(event) {
+
+    document.querySelector(".starbucksimage").src = event;
+
+    // let images = document.querySelectorAll(".thumb ul li img");
+
+    // console.log(images);
+
+    // images.forEach(function (event) {
+    //     event.addEventListener("click", function () {
+    //         document.querySelector(".starbucksimage").src = event.src;
+    //     });
+    // });
+}
